457e7b2d274d5c3dbe67e25ae3e5dbace7fcd550
abril-gonzalez <167778141+abril-gonzalez@users.noreply.github.com>
2025-05-19 19:09:14 -0300

    Places

---
+++ src/components/DateContainer.jsx
@@ -0,0 +1,18 @@
+import "../styles/DateContainer.css";
+
+export default function DateContainer() {
+  return (
+    <div className="container-one-date">
+      {/* <div className="container-title">
+        <h2>Te esperamos para celebrar nuestra boda</h2>
+        </div> */}
+      <div className="container-date">
+        <div className="subcontainer-date">
+          <h5>SABADO</h5>
+          <h2>28</h2>
+          <h5>AGOSTO</h5>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/containers/Places.jsx
+++ src/containers/Places.jsx
@@ -1,42 +1,71 @@
 import React from "react";
-import iglesia from "../assets/img/2.png";
+import florLinea1 from "../assets/img/flor-linea-1.png";
+import florLinea2 from "../assets/img/flor-linea-2.png";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import "../styles/Places.css";
 import Curve from "../components/Curve";
+import DateContainer from "../components/DateContainer";
 
 export default function Places() {
   return (
-    <div className="container-one-places">
-      {/* <Curve curve={1} /> */}
-      {/* <div className="container-title">
-        <h2>Te esperamos para celebrar nuestra boda</h2>
-      </div> */}
-      <div className="container-date">
-        <div className="subcontainer-date">
-          <h5>SABADO</h5>
-          <h2>28</h2>
-          <h5>AGOSTO</h5>
-        </div>
-      </div>
-      <div className="container-two-place">
-        <div className="subcontainer-two-place">
-          <h3>CEREMONIA CIVIL Y FIESTA</h3>
-          <div className="container-data">
+    <div>
+      <DateContainer />
+      <div className="container-one-places">
+        <Curve curve={1} />
+        <div className="container-two-place">
+          <div className="container-individual">
             <div>
-            <img className="container-img" src={iglesia} alt="" />
+              <h4>
+                A
+              </h4>
             </div>
+            <div className="line"></div>
             <div>
-              <h4>Salon de Eventos Hualilan</h4>
-              <h4>22:00 hs</h4>
-              <div className="link-place">
-                <FaMapMarkerAlt className="icon-map"/>
-                <a className="btn-a" href="https://maps.app.goo.gl/4sGbo9hjKyK97EGz5">Como Llegar</a>
-              </div>
+              <img className="container-img-2" src={florLinea1} alt="" />
             </div>
           </div>
+          <div className="container-individual">
+            <div>
+              <img className="container-img-1" src={florLinea2} alt="" />
+            </div>
+            <div className="line"></div>
+            <div>
+              <h4>
+                W
+              </h4>
+            </div>
+          </div>
+          <div className="container-individual">
+            <div></div>
+            <div className="line"></div>
+            <div>
+              <h2>data</h2>
+            </div>
+          </div>
+          {/* <div className="subcontainer-two-place">
+            <h3>CEREMONIA CIVIL Y FIESTA</h3>
+            <div className="container-data">
+              <div>
+                <img className="container-img" src={iglesia} alt="" />
+              </div>
+              <div>
+                <h4>Salon de Eventos Hualilan</h4>
+                <h4>22:00 hs</h4>
+                <div className="link-place">
+                  <FaMapMarkerAlt className="icon-map" />
+                  <a
+                    className="btn-a"
+                    href="https://maps.app.goo.gl/4sGbo9hjKyK97EGz5"
+                  >
+                    Como Llegar
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div> */}
         </div>
+        <Curve curve={0} />
       </div>
-      <Curve curve={0} />
     </div>
   );
 }

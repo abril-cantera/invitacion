ff46d2c7358f58473c905049946ac2124a329af3
abril-gonzalez <167778141+abril-gonzalez@users.noreply.github.com>
2025-05-19 17:45:36 -0300

    Places

--- src/components/Curve.jsx
+++ src/components/Curve.jsx
@@ -8,7 +8,7 @@ export default function Curve({ curve }) {
     <div className={curvePosition}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
-          fill="#f6f7ec"
+          fill="#ffffff"
           fillOpacity="1"
           d="M0,64L80,101.3C160,139,320,213,480,213.3C640,213,800,139,960,128C1120,117,1280,171,1360,197.3L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
         ></path>

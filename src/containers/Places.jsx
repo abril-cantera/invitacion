b2c68a2224e30c493266110e3d7e6f8e0c5aaac5
abril-gonzalez <167778141+abril-gonzalez@users.noreply.github.com>
2025-05-21 17:23:43 -0300

    Places

--- src/containers/Places.jsx
+++ src/containers/Places.jsx
@@ -15,22 +15,28 @@ export default function Places() {
       <div className="container-decorartion-places">
         <Curve curve={1} />
         <div className="subcontainer-decoration">
-          <img className="flor" src={flor} alt="" />
-          <img src={aw} alt="" />
-          <img className="down-flor" src={flor} alt="" />
-        </div>
-        <div className="subcontainer-places">
-          <div>
-            <h4>Salon de Eventos Hualilan</h4>
-            <h4>22:00 hs</h4>
-            <div className="link-place">
-              <FaMapMarkerAlt className="icon-map" />
-              <a
-                className="btn-a"
-                href="https://maps.app.goo.gl/4sGbo9hjKyK97EGz5"
-              >
-                Como Llegar
-              </a>
+          <div className="aw-wrapper">
+            <img className="flor flor-top" src={flor} alt="Decoración floral" />
+            <img className="aw-img" src={aw} alt="a | w" />
+            <img
+              className="flor flor-bottom"
+              src={flor}
+              alt="Decoración floral"
+            />
+            <div className="subcontainer-places">
+              <div>
+                <h4>Salon de Eventos Hualilan</h4>
+                <h4>22:00 hs</h4>
+                <div className="link-place">
+                  <FaMapMarkerAlt className="icon-map" />
+                  <a
+                    className="btn-a"
+                    href="https://maps.app.goo.gl/4sGbo9hjKyK97EGz5"
+                  >
+                    Como Llegar
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
